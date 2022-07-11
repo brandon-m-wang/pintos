13d5974e9ac66d9bb5c238225f6885682b1806d1
Brandon Wang <brandonwang0503@berkeley.edu>
2022-07-11 00:31:15 -0700

    task 2

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -96,6 +96,8 @@ struct thread {
 #ifdef USERPROG
   /* Owned by process.c. */
   struct process* pcb; /* Process control block if this thread is a userprog */
+  struct process_fields *process_fields;
+  struct list children;
 #endif
 
   /* Owned by thread.c. */
