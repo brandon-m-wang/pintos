537fadc4ba7e307119575732898e365f9992857d
Brandon Wang <brandonwang0503@berkeley.edu>
2022-07-12 16:46:32 -0700

    test if main thread fails

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -116,7 +116,6 @@ void thread_init(void) {
   init_thread(initial_thread, "main", PRI_DEFAULT);
   initial_thread->status = THREAD_RUNNING;
   initial_thread->tid = allocate_tid();
-  initial_thread->process_fields = malloc(sizeof(struct process_fields));
 }
 
 /* Starts preemptive thread scheduling by enabling interrupts.
