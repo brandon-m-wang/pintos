7694731ad93564b792dc0ffa5cc890a1a0c1d90c
Brandon Wang <brandonwang0503@berkeley.edu>
2022-08-13 21:09:34 -0700

    success true

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -140,7 +140,7 @@ void rollback(struct list *allocated_sectors) {
    Returns false if memory or disk allocation fails. */
 bool inode_create(block_sector_t sector, off_t length) {
   struct inode_disk* disk_inode = NULL;
-  bool success = false;
+  bool success = true;
 
   ASSERT(length >= 0);
 
